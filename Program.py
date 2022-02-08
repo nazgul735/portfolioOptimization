e9e737be021f56d06059e8651189998077e33943
nazgul735 <68790240+nazgul735@users.noreply.github.com>
2022-02-08 19:51:11 +0100

    screening daily returns added

--- Program.py
+++ Program.py
@@ -1,14 +1,15 @@
+from turtle import title
 import matplotlib.pyplot as plt
 from datetime import datetime
 import numpy as np
-import pandas_datareader as pdr
-pdr.get_data_fred('GS10')
+from pandas_datareader import data as web
+import pandas as pd
 plt.style.use('fivethirtyeight')
 
 portfolio = ['BABA', 'FB', 'GEVO']
-weight = np.arrawy([0.2, 0.2, 0.2])
+weight = np.array([0.2, 0.2, 0.2])
 firstInvestment = '2022-01-01'
-today = datetime.today().strftime('%Y-%m-d%')
+today = datetime.today().strftime('%Y-%m-%d')
 
 
 df = pd.DataFrame()
@@ -18,3 +19,19 @@ for stock in portfolio:
         stock, data_source='yahoo', start=firstInvestment, end=today)['Adj Close']
 
 df
+
+portfTitle = 'Portfolio Adj. Closing Prices'
+
+theStocks = df
+for i in theStocks.column.values:
+    plt.plot(theStocks[i], label=i)
+
+plt.title(title)
+plt.xlabel('date', fontsize=12)
+plt.ylabel('Adj price', fontsize=12)
+
+plt.legend(theStocks.columns.values, loc='upper left')
+plt.show()
+
+dailyReturns = df.pct_change()
+dailyReturns
