9d30627512b10a006581cc1917bcf03a838bda6d
nazgul735 <68790240+nazgul735@users.noreply.github.com>
2022-02-08 18:39:38 +0100

    added df for visualizing portfolio

--- Program.py
+++ Program.py
@@ -1,7 +1,20 @@
-from pandas_datareader import data as web
-import pandas as pd
-import numpy as np 
-from datetime import datetime 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+from datetime import datetime
+import numpy as np
+import pandas_datareader as pdr
+pdr.get_data_fred('GS10')
 plt.style.use('fivethirtyeight')
 
+portfolio = ['BABA', 'FB', 'GEVO']
+weight = np.arrawy([0.2, 0.2, 0.2])
+firstInvestment = '2022-01-01'
+today = datetime.today().strftime('%Y-%m-d%')
+
+
+df = pd.DataFrame()
+
+for stock in portfolio:
+    pd[stock] = web.DataReader(
+        stock, data_source='yahoo', start=firstInvestment, end=today)['Adj Close']
+
+df
